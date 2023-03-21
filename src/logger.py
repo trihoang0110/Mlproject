2d8f9d920c40c8b755b2887ef7ff1da05cc9ff18
Hoàng Trí <trihoang301100@gmail.com>
2023-03-21 23:15:03 +0700

    data ingestion

--- src/logger.py
+++ src/logger.py
@@ -3,7 +3,10 @@ import os
 from datetime import datetime
 
 LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+logs_dir = os.path.join(os.getcwd(),"logs")
+os.makedirs(logs_dir, exist_ok=True)
+logs_path=os.path.join(logs_dir,LOG_FILE)
+
 
 logger = logging.getLogger(__name__)
 handler = logging.FileHandler(logs_path)
